9e1f02357fda3f0b9eae015a14574eeb60332fd2
David Arakelyan <da378@scarletmail.rutgers.edu>
2015-11-23 01:00:28 -0500

    Uploaded Changes to KADAPT - Dave

--- BAssignments/B2/KADAPT-Dave/Assets/MyBehaviorTree.cs
+++ BAssignments/B2/KADAPT-Dave/Assets/MyBehaviorTree.cs
@@ -4,18 +4,23 @@ using TreeSharpPlus;
 
 public class MyBehaviorTree : MonoBehaviour
 {
-	public Transform wander1;
-	public Transform wander2;
-	public Transform wander3;
+	//public Transform wander1;
+	//public Transform wander2;
+	//public Transform wander3;
+    public GameObject CRIMINAL;
 	public GameObject participant;
+    public GameObject COP;
+    private BehaviorAgent behaviorAgent;
+    public GameObject button;
+    public GameObject Door;
 
-	private BehaviorAgent behaviorAgent;
 	// Use this for initialization
 	void Start ()
 	{
 		behaviorAgent = new BehaviorAgent (this.BuildTreeRoot ());
 		BehaviorManager.Instance.Register (behaviorAgent);
 		behaviorAgent.StartBehavior ();
+
 	}
 	
 	// Update is called once per frame
@@ -30,13 +35,70 @@ public class MyBehaviorTree : MonoBehaviour
 		return new Sequence( participant.GetComponent<BehaviorMecanim>().Node_GoTo(position), new LeafWait(1000));
 	}
 
-	protected Node BuildTreeRoot()
+    protected Node ApproachAndStare(Transform target)
+    {
+        Val<Vector3> position = Val.V(() => target.position);
+        return new Sequence(COP.GetComponent<BehaviorMecanim>().Node_OrientTowards(position), new LeafWait(1000));
+    }
+
+    protected Node ApproachAndStare2(Transform target)
+    {
+        Val<Vector3> position = Val.V(() => target.position);
+        return new Sequence(CRIMINAL.GetComponent<BehaviorMecanim>().Node_OrientTowards(position), new LeafWait(1000));
+    }
+
+    protected Node HandsUp(Transform target)
+    {
+        Val<Vector3> position = Val.V(() => target.position);
+        return new Sequence(CRIMINAL.GetComponent<BehaviorMecanim>().Node_BodyAnimation("DYING", true), new LeafWait(1000));
+    }
+
+    /*
+    protected Node CriminalCrouchesNearCop(Transform target)
+    {
+        Val<Vector3> position = Val.V(() => target.position);
+        return new Sequence(CRIMINAL.GetComponent<BehaviorMecanim>().Node_BodyAnimation("DYING", true), new LeafWait(1000));
+    }
+    */
+
+    protected Node CopEngageCriminal (Transform target)
+    {
+        Val<Vector3> position = Val.V(() => target.position);
+        return new Sequence(COP.GetComponent<BehaviorMecanim>().Node_HandAnimation("PISTOLAIM", true), new LeafWait(1000));
+    }
+
+    protected Node ApproachButton(Transform target)
+    {
+        Val<Vector3> position = Val.V(() => target.position);
+        return new Sequence(COP.GetComponent<BehaviorMecanim>().Node_GoTo(position), new LeafWait(1000));
+    }
+
+    protected Node PressButton(Transform target)
+    {
+        Val<Vector3> position = Val.V(() => target.position);
+        return new Sequence(COP.GetComponent<BehaviorMecanim>().Node_BodyAnimation("FIGHT", true), new LeafWait(1000));
+    }
+    
+    protected Node BuildTreeRoot()
 	{
-		return
-			new DecoratorLoop(
-				new SequenceShuffle(
-					this.ST_ApproachAndWait(this.wander1),
-					this.ST_ApproachAndWait(this.wander2),
-					this.ST_ApproachAndWait(this.wander3)));
-	}
+
+
+
+        return new DecoratorLoop(
+
+            new Sequence(
+                //this.ST_ApproachAndWait(this.wander1),
+
+                this.ApproachAndStare2(this.COP.transform)));
+                    //this.ApproachAndStare2(this.COP.transform),
+                   // this.CopEngageCriminal(this.CRIMINAL.transform),
+                    //this.HandsUp(this.COP.transform)));
+                    //this.ApproachButton(this.button.transform),
+                    //this.ApproachAndStare(this.CRIMINAL.transform),
+                   // this.PressButton(this.COP.transform)));
+        //this.ST_ApproachAndWait(this.wander2),
+        //this.ST_ApproachAndWait(this.wander3)));
+        
+    }
+    
 }
---
+++ BAssignments/B2/KADAPT-Dave/Assets/RayCasting.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using TreeSharpPlus;
+
+public class RayCasting : MonoBehaviour {
+    NavMeshAgent agent;
+    private GameObject agentToMove;
+    private BehaviorAgent behaviorAgent;
+    public GameObject COP;
+    public GameObject CRIMINAL;
+
+    // Use this for initialization
+    void Start () {
+        behaviorAgent = new BehaviorAgent(this.BuildTreeRoot());
+        BehaviorManager.Instance.Register(behaviorAgent);
+        behaviorAgent.StartBehavior();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        Debug.DrawRay(ray.origin, ray.direction * 60, Color.cyan);
+
+        /*
+        if(Input.GetButtonDown("Fire1"))
+        {
+            if(Physics.Raycast(ray,out hit) == true)
+            {
+                if (hit.transform.gameObject.CompareTag("Cop"))
+                {
+                Debug.Log("~~ Cop Selected ~~");
+                string name = hit.collider.gameObject.name;
+                }
+                
+            }
+        }
+        */
+        
+        if (Input.GetButtonDown("Fire2") || Input.GetButton("Fire2"))
+        {
+            if (Physics.Raycast(ray, out hit) == true)
+            {
+                if(hit.transform.gameObject.CompareTag("Ground"))
+                {
+                    agentToMove = GameObject.Find("Cop");
+                    agentToMove.GetComponent<NavMeshAgent>().SetDestination(hit.point);
+                }
+
+            }
+        }
+        
+
+    }
+
+    protected Node KickCriminal(Transform target)
+    {
+        Val<Vector3> position = Val.V(() => target.position);
+        return new Sequence(COP.GetComponent<BehaviorMecanim>().Node_BodyAnimation("FIGHT", true), new LeafWait(1000));
+    }
+
+
+    protected Node GunIdle(Transform target)
+    {
+        Val<Vector3> position = Val.V(() => target.position);
+        
+            return new Sequence(COP.GetComponent<BehaviorMecanim>().Node_HandAnimation("PISTOLAIM", true), new LeafWait(1000));
+       
+    }
+
+    protected Node BuildTreeRoot()
+    {
+
+            return new DecoratorLoop( 
+            new Sequence(this.GunIdle(this.COP.transform)));
+            
+        
+    }
+}
