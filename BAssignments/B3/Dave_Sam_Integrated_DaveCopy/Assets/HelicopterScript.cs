4004ffdd4652ddcae682f7feac221de019fa93ed
David Arakelyan <da378@scarletmail.rutgers.edu>
2015-12-18 23:35:39 -0500

    Final Copy

---
+++ BAssignments/B3/Dave_Sam_Integrated_DaveCopy/Assets/HelicopterScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelicopterScript : MonoBehaviour {
+    public GameObject Sam;
+    public GameObject Dave;
+    public GameObject Helicopter;
+    public Camera camera1;
+    public Camera camera2;
+
+    private Rigidbody HeliCollision;
+
+    Animator helicopteranimator;
+    // Use this for initialization
+	void Start () {
+        helicopteranimator = Helicopter.GetComponent<Animator>();
+        HeliCollision = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	
+    void OnTriggerEnter(Collider Other)
+    {
+            Debug.Log("Collision Detected");
+            if (Other.gameObject.name == "Sam")
+            {
+            camera1.enabled = true;
+            //camera2.enabled = false;
+            Sam.SetActive(false);
+            Dave.SetActive(false);
+            helicopteranimator.SetTrigger("HelicopterTakeOff");
+            }
+        }
+
+
+    
+}
